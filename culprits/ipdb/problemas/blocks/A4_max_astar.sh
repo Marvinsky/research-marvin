8c05c5a9bdc0281abc1970284d210b21da8be1fc
marvin <nivramsky@gmail.com>
2015-03-12 10:30:35 -0700

    12/03/15 test for max and min added

---
+++ culprits/ipdb/problemas/blocks/A4_max_astar.sh
@@ -0,0 +1,31 @@
+#PBS -N culprits_d1_p1
+
+#PBS -m b
+
+#PBS -M marvin.zarate@ufv.br
+
+cd $PBS_O_WORKDIR
+
+source /usr/share/modules/init/bash
+
+module load python
+module load mercurial
+
+RESULTS=/home/marvin/marvin/culprits/ipdb/problemas/blocks/resultado
+
+cd /home/marvin/fd-culprits
+
+python3 src/translate/translate.py benchmarks/blocks/domain.pddl benchmarks/blocks/probBLOCKS-5-0.pddl Astarblocks1  blocks probBLOCKS-5-0.pddl  ipdb
+
+src/preprocess/preprocess < Astarblocks1.sas
+
+src/search/downward-release --global_probes 100  --domain_name blocks --problem_name probBLOCKS-5-0.pddl --heuristic_name ipdb --search "astar_original(max([ipdb(), hmax(), merge_and_shrink()]))" <  Astarblocks1 >> ${RESULTS}/probBLOCKS-5-0-max
+
+
+
+rm Astarblocks1
+
+
+
+rm Astarblocks1.sas
+
