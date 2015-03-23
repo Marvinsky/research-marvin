5db01b9437b5ce5f89b0496011ca00018c3917d0
marvin <nivramsky@gmail.com>
2015-03-23 06:28:33 -0700

    23/03/15 Adding changes for culprits and dfs

---
+++ testdfs/ipdb/problemas_culprits/blocks/A6.sh
@@ -0,0 +1,37 @@
+#PBS -N ipdb_d1_p6
+
+#PBS -m b
+
+#PBS -M marvin.zarate@ufv.br
+
+#PBS -l nodes=1:ppn=1
+
+#PBS -l walltime=1800
+
+#PBS -l pmem=6gb
+
+cd $PBS_O_WORKDIR
+
+source /usr/share/modules/init/bash
+
+module load python
+module load mercurial
+
+RESULTS=/home/marvin/marvin/testdfs/ipdb/problemas_culprits/blocks/resultado
+
+cd /home/marvin/fd-culprits
+
+python3 src/translate/translate.py benchmarks/blocks/domain.pddl benchmarks/blocks/probBLOCKS-5-2.pddl Astarblocks6  blocks  probBLOCKS-5-2.pddl  ipdb
+
+src/preprocess/preprocess < Astarblocks6.sas
+
+src/search/downward-release --global_probes 100 --domain_name blocks --problem_name probBLOCKS-5-2.pddl --heuristic_name ipdb  --search "dfs_culprits(ipdb())" <  Astarblocks6 >> ${RESULTS}/probBLOCKS-5-2.pddl
+
+
+
+rm Astarblocks6
+
+
+
+rm Astarblocks6.sas
+
