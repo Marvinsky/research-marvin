0dfa5d6d2def19e4d1c275249c518532fb959a7c
marvin <nivramsky@gmail.com>
2015-04-09 21:34:18 -0300

    09/04/15 Modifying barman_op11

--- testss/ipdb/problemas/barman-opt11-strips/A15.sh
+++ testss/ipdb/problemas/barman-opt11-strips/A15.sh
@@ -1,8 +1,14 @@
-#PBS -N _ssbarman_15
+#PBS ss_15
 
-#PBS -m a
+#PBS -m b
 
 #PBS -M marvin.zarate@ufv.br
+
+#PBS -l nodes=1:ppn=1
+
+#PBS -l walltime=1800
+
+#PBS -l pmem=6gb
 
 cd $PBS_O_WORKDIR
 
@@ -11,9 +17,9 @@ source /usr/share/modules/init/bash
 module load python
 module load mercurial
 
-RESULTS=/home/levi/marvin/marvin/testss/ipdb/problemas/barman-opt11-strips/resultado
+RESULTS=/home/marvin/marvin/testss/ipdb/problemas/barman-opt11-strips/resultado
 
-cd /home/levi/marvin/fd
+cd /home/marvin/fd
 
 python3 src/translate/translate.py benchmarks/barman-opt11-strips/domain.pddl benchmarks/barman-opt11-strips/pfile04-015.pddl Astarbarman-opt11-strips15  barman-opt11-strips  pfile04-015.pddl  ipdb
 
