cc0d188d87f03a0d5d417d954d1c672b42b35b34
marvin <nivramsky@gmail.com>
2015-04-16 19:55:54 -0300

    16/04/15 Adding fdist for tidybot modifying the F_boundary up to the max get from A*

--- testss/ipdb/problemas/tidybot-opt11-strips/A1.sh
+++ testss/ipdb/problemas/tidybot-opt11-strips/A1.sh
@@ -21,11 +21,11 @@ RESULTS=/home/marvin/marvin/testss/ipdb/problemas/tidybot-opt11-strips/resultado
 
 cd /home/marvin/fd
 
-python3 src/translate/translate.py benchmarks/tidybot-opt11-strips/domain.pddl benchmarks/tidybot-opt11-strips/p10.pddl Astartidybot-opt11-strips1  tidybot-opt11-strips  p10.pddl  ipdb
+python3 src/translate/translate.py benchmarks/tidybot-opt11-strips/domain.pddl benchmarks/tidybot-opt11-strips/p01.pddl Astartidybot-opt11-strips1  tidybot-opt11-strips  p01.pddl  ipdb
 
 src/preprocess/preprocess < Astartidybot-opt11-strips1.sas
 
-src/search/downward-release --global_probes 1000 --domain_name tidybot-opt11-strips --problem_name p10.pddl --heuristic_name ipdb --search "ss(ipdb())" <  Astartidybot-opt11-strips1 > ${RESULTS}/p10.pddl
+src/search/downward-release --global_probes 1000 --domain_name tidybot-opt11-strips --problem_name p01.pddl --heuristic_name ipdb --search "ss(ipdb(max_time=600))" <  Astartidybot-opt11-strips1 > ${RESULTS}/p01.pddl
 
 
 
---
+++ testss/ipdb/problemas/tidybot-opt11-strips/A9.sh
@@ -0,0 +1,37 @@
+#PBS ss_9
+
+#PBS -m b
+
+#PBS -M marvin.zarate@ufv.br
+
+#PBS -l nodes=1:ppn=1
+
+#PBS -l walltime=1800
+
+#PBS -l pmem=6gb
+
+cd $PBS_O_WORKDIR
+
+source /usr/share/modules/init/bash
+
+module load python
+module load mercurial
+
+RESULTS=/home/marvin/marvin/testss/ipdb/problemas/tidybot-opt11-strips/resultado
+
+cd /home/marvin/fd
+
+python3 src/translate/translate.py benchmarks/tidybot-opt11-strips/domain.pddl benchmarks/tidybot-opt11-strips/p09.pddl Astartidybot-opt11-strips9  tidybot-opt11-strips  p09.pddl  ipdb
+
+src/preprocess/preprocess < Astartidybot-opt11-strips9.sas
+
+src/search/downward-release --global_probes 1000 --domain_name tidybot-opt11-strips --problem_name p09.pddl --heuristic_name ipdb --search "ss(ipdb(max_time=600))" <  Astartidybot-opt11-strips9 > ${RESULTS}/p09.pddl
+
+
+
+rm Astartidybot-opt11-strips9
+
+
+
+rm Astartidybot-opt11-strips9.sas
+
