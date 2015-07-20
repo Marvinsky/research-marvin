87fb3288a614a904576d037b17614fc583374374
marvin <nivramsky@gmail.com>
2015-07-20 19:24:18 -0300

    20/06/15 jpeg modified

--- reports/global_exp_2_ss_astar_v3_1/generatePlot.R
+++ reports/global_exp_2_ss_astar_v3_1/generatePlot.R
@@ -13,17 +13,11 @@ x <- instance$V1
 y <- instance$V2
 
 #plot(x, y, col.axis = "sky blue", col.lab = "thistle", log = "xy", pch = 4, col = "blue")
-plot(x, y, col.axis = "sky blue", col.lab = "thistle", pch = 4, col = "blue")
+
+all = c(x,y)
+range = c(min(all), max(all))
+plot(x, y, xlim=range, ylim=range, col.axis = "sky blue", col.lab = "thistle", pch = 4, col = "blue")
+
 model <- lm(y ~ x)
 abline(model)
 title(main = "SS vs A*", xlab = "SS", ylab = "A*")
-
-#draw the line that best fitst the behavior of the results
-
-#abline(z) # equivalent to abline(reg = z) or
-#abline(coef = coef(z))
-
-
-## draw arrows from point to point :
-#s <- seq(length(x)-1)  # one shorter than data
-#arrows(x[s], y[s], x[s+1], y[s+1], col= 'green')
