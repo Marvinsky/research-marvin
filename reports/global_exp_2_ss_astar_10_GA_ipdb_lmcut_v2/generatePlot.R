acfb10cad4dc047df183a15b6e803cb158644ed0
marvin <nivramsky@gmail.com>
2015-08-18 14:37:40 -0300

    18/08/15 update v2

--- reports/global_exp_2_ss_astar_10_GA_ipdb_lmcut_v2/generatePlot.R
+++ reports/global_exp_2_ss_astar_10_GA_ipdb_lmcut_v2/generatePlot.R
@@ -33,58 +33,60 @@ for (m in 1:21) {
     #family <-as.factor()
     all_rows <- read.table(dump_name)
     total_rows <- nrow(all_rows)
-    header <- read.table(dump_name, skip = total_rows -1 , nrow = 1)
-    instances_solved <- header$V2
-    print(instances_solved)
+    if (total_rows > 1) {
+      header <- read.table(dump_name, skip = total_rows -1 , nrow = 1)
+      instances_solved <- header$V2
+      print(instances_solved)
     
-    instance <- read.table(rname, nrow = total_rows - 1)
-    summary(instance) 
-    print(instance)
+      instance <- read.table(rname, nrow = total_rows - 1)
+      summary(instance) 
+      print(instance)
     
-    x <- instance$V1
-    y <- instance$V2
-    family <- instance$V3
+      x <- instance$V1
+      y <- instance$V2
+      family <- instance$V3
     
-    all = c(x,y)
-    range = c(min(all), max(all))
+      all = c(x,y)
+      range = c(min(all), max(all))
     
-    s1 <- c(rname)
-    n1 <- nchar(rname) - 4
-    s3 <- substring(s1, first=1, last=n1)
+      s1 <- c(rname)
+      n1 <- nchar(rname) - 4
+      s3 <- substring(s1, first=1, last=n1)
     
-    s2 <- ".jpeg"
-    mname <-paste(s3, s2, sep = "")
-    print(mname)
+      s2 <- ".jpeg"
+      mname <-paste(s3, s2, sep = "")
+      print(mname)
     
-    jpeg(filename=mname,  width = 480, height = 480, units = "px")
+      jpeg(filename=mname,  width = 480, height = 480, units = "px")
     
-    #ipdb = blue
-    #lmcut = red
-    #gapdb = green
-    plot(x, y, col = ifelse(family == 'ipdb', 'blue', ifelse(family == 'lmcut', 'red', 'green')), xlim=range, ylim=range, bty = "n", pch = 4, col.axis = "sky blue", col.lab = "thistle")
-    #plot(x, y, xlim=range, ylim=range, bty = "n", col.axis = "sky blue", col.lab = "thistle", pch = 4, col = "blue")
+      #ipdb = blue
+      #lmcut = red
+      #gapdb = green
+      plot(x, y, col = ifelse(family == 'ipdb', 'blue', ifelse(family == 'lmcut', 'red', 'green')), xlim=range, ylim=range, bty = "n", pch = 4, col.axis = "sky blue", col.lab = "thistle")
+      #plot(x, y, xlim=range, ylim=range, bty = "n", col.axis = "sky blue", col.lab = "thistle", pch = 4, col = "blue")
     
-    abline(a = 0, b = 1)  # y = x
-    abline(v = 2, col = "gray60") # x = 2
+      abline(a = 0, b = 1)  # y = x
+      abline(v = 2, col = "gray60") # x = 2
     
-    leg.txt <- c("blue = ipdb", "red = lmcut", "green = gapdb")
-    minx <- min(all)
-    maxy <- max(all)
+      leg.txt <- c("blue = ipdb", "red = lmcut", "green = gapdb")
+      minx <- min(all)
+      maxy <- max(all)
     
-    legend(list(x = minx, y = maxy),
-          legend = leg.txt,
-          pch = 4,
-          bty = "o",
-          col = c("blue", "red", "green")
-          )
+      legend(list(x = minx, y = maxy),
+            legend = leg.txt,
+            pch = 4,
+            bty = "o",
+            col = c("blue", "red", "green")
+            )
     
     
-    title <- "SS vs A* "
-    problem <- paste(s3, instances_solved, sep = " : ")
-    finalTitle <- paste(title, problem, sep="- ")
+      title <- "SS vs A* "
+      problem <- paste(s3, instances_solved, sep = " : ")
+      finalTitle <- paste(title, problem, sep="- ")
     
-    title(main = finalTitle, xlab = "SS", ylab = "A*")
+      title(main = finalTitle, xlab = "SS", ylab = "A*")
     
-    dev.off()
+      dev.off()
+    }
   }
 }
