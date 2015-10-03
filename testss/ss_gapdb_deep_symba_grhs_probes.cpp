a66a69a7b33bf6f40fc7651ceb61ab9088f28638
marvin <nivramsky@gmail.com>
2015-10-03 00:20:52 -0300

    03/10/15 add ss_gapdb_deep_symba_grhs_probes

--- testss/ss_gapdb_deep_symba_grhs_probes.cpp
+++ testss/ss_gapdb_deep_symba_grhs_probes.cpp
@@ -34,14 +34,228 @@ std::string exec(const char* cmd) {
     return result;
 }
 
-string currentDateTime() {
-	time_t now = time(0);
-	struct tm tstruct;
-	char buf[80];
-	tstruct = *localtime(&now);
+int toDecimal(int n_no) {
+	int n;
+	switch (n_no) {
+		case 11:
+			n = 1;
+			break;
+		case 12:
+			n = 2;
+			break;
+		case 13:
+			n = 3;
+			break;
+		case 14:
+			n = 4;
+			break;
+		case 15:
+			n = 5;
+			break;
+		case 16:
+			n = 6;
+			break;
+		case 17:
+			n = 7;
+			break;
+		case 18:
+			n = 8;
+			break;
+		case 19:
+			n = 9;
+			break;
+		case 20:
+			n = 10;
+			break;
+		case 21:
+			n = 11;
+			break;
+		case 22:
+			n = 12;
+			break;
+		case 23:
+			n = 13;
+			break;
+		case 24:
+			n = 14;
+			break;
+		case 25:
+			n = 15;
+			break;
+		case 26:
+			n = 16;
+			break;
+		case 27:
+			n = 17;
+			break;
+		case 28:
+			n = 18;
+			break;
+		case 29:
+			n = 19;
+			break;
+		case 30:
+			n = 20;
+			break;
+		default:
+			n = 0;
+			break;
+	}
+	return n;
+}
+
+int getVisitAllInt(string instance) {
+	int n;
+	if (instance == "problem02-full.pddl") {
+		n = 1;
+	} else if (instance == "problem02-half.pddl") {
+		n = 2;
+	} else if (instance == "problem03-full.pddl") {
+		n = 3;
+	} else if (instance == "problem03-half.pddl") {
+		n = 4;
+	} else if (instance == "problem04-full.pddl") {
+		n = 5;
+	} else if (instance == "problem04-half.pddl") {
+		n = 6;
+	} else if (instance == "problem05-full.pddl") {
+		n = 7;
+	} else if (instance == "problem05-half.pddl") {
+		n = 8;
+	} else if (instance == "problem06-full.pddl") {
+		n = 9;
+	} else if (instance == "problem06-half.pddl") {
+		n = 10;
+	} else if (instance == "problem07-full.pddl") {
+		n = 11;
+	} else if (instance == "problem07-half.pddl") {
+		n = 12;
+	} else if (instance == "problem08-full.pddl") {
+		n = 13;
+	} else if (instance == "problem08-half.pddl") {
+		n = 14;
+	} else if (instance == "problem09-full.pddl") {
+		n = 15;
+	} else if (instance == "problem09-half.pddl") {
+		n = 16;
+	} else if (instance == "problem10-full.pddl") {
+		n = 17;
+	} else if (instance == "problem10-half.pddl") {
+		n = 18;
+	} else if (instance == "problem11-full.pddl") {
+		n = 19;
+	} else if (instance == "problem11-half.pddl") {
+		n = 20;
+	} else {
+		n = 0;
+	}
+	return n;
+}
+
+string getSimplePXX(string instance, string FINAL_NAME) {
+	string result;
+	string s = instance;
+        size_t t = s.find(".");
+        string p_number = s.substr(0, t);
+        size_t t2 = p_number.find("p");
+        string number = p_number.substr(t2 +1, p_number.length());
+        int n = atoi(number.c_str());
+        ostringstream convert_number;
+        convert_number << n;
+        string Result_n = convert_number.str();
+        string globalpxx = FINAL_NAME;
+        globalpxx += Result_n;
+        result = globalpxx;
+	return result;
+}
+
+string translator(string key, string instance) {
+	cout<<"key="<<key<<"\n";
+	string result;
+	cout<<"instance="<<instance<<"\n";
+        if (key == "barman-opt11-strips") {
+                string s = instance;
+                size_t t = s.find("-");
+                string number_pddl = s.substr(t + 1, s.length());
+		size_t t2 = number_pddl.find(".");
+		string number = number_pddl.substr(0, t2);
+		int n = atoi(number.c_str());
+		ostringstream convert_number;
+                convert_number << n;
+                string Result_n = convert_number.str();	
+		string barman = "BARMAN-";
+		barman += Result_n;
+		result = barman;
+        } else if (key == "elevators-opt11-strips") {
+		result = getSimplePXX(instance, "ELEVATORS-");	
+        } else if (key == "floortile-opt11-strips") {
+		string s = instance;
+		string delimiter = "-";
+                string pot[6];
+                size_t pos = 0;
+                string token;
+                int index = 0;
+                while ((pos = s.find(delimiter)) != std::string::npos) {
+                	token = s.substr(0, pos);
+                        //cout<<"token="<<token<<"\n";
+                        pot[index] = token;
+                        s.erase(0, pos + delimiter.length());
+                        index++;
+                }
+                pot[index] = s;
+		string number_pddl = pot[2];
+		size_t t2 = number_pddl.find(".");
+		string number = number_pddl.substr(0, t2);
+		int n = atoi(number.c_str());
+		ostringstream convert_number;
+                convert_number << n;
+                string Result_n = convert_number.str();	
+		string floortile = "FLOORTILE-";
+		floortile += Result_n;
+		result = floortile;
+        } else if (key == "nomystery-opt11-strips") {
+		result = getSimplePXX(instance, "NOMYSTERY-");
+        } else if (key == "openstacks-opt11-strips") {
+		result = getSimplePXX(instance, "OPENSTACKS-");
+        } else if (key == "parcprinter-opt11-strips") {
+		result = getSimplePXX(instance, "PARCPRINTER-");
+        } else if (key == "parking-opt11-strips") {
+		string s = instance;
+                size_t t = s.find("-");
+                string number_pddl = s.substr(t + 1, s.length());
+		size_t t2 = number_pddl.find(".");
+		string number = number_pddl.substr(0, t2);
+		int n_no_translate = atoi(number.c_str());
+		int n = toDecimal(n_no_translate);
+		ostringstream convert_number;
+                convert_number << n;
+                string Result_n = convert_number.str();	
+		string parking = "PARKING-";
+		parking += Result_n;
+		result = parking;
+        } else if (key == "pegsol-opt11-strips") {
+		result = getSimplePXX(instance, "PEGSOL-");
+        } else if (key == "scanalyzer-opt11-strips") {	
+		result = getSimplePXX(instance, "SCANALYZER-");
+        } else if (key == "sokoban-opt11-strips") {	
+		result = getSimplePXX(instance, "SOKOBAN-");
+        } else if (key == "tidybot-opt11-strips") {
+		result = getSimplePXX(instance, "TIDYBOT-");
+        } else if (key == "transport-opt11-strips") {	
+		result = getSimplePXX(instance, "TRANSPORT-");
+        } else if (key == "visitall-opt11-strips") {
+		int n = getVisitAllInt(instance);
+		ostringstream convert_number;
+                convert_number << n;
+                string Result_n = convert_number.str();	
+		string visitall = "VISITALL-";
+		visitall += Result_n;
+		result = visitall;
 	
-	strftime(buf, sizeof(buf), "%Y-%m-%d", &tstruct);
-	return buf;
+        } else if (key == "woodworking-opt11-strips") {	
+		result = getSimplePXX(instance, "WOODWORKING-");
+        }
+        return result;
 }
 
 int getTotalLevels(string interText) {
@@ -100,7 +314,10 @@ void create_sh(string pasta, string dominio, string problema, int num_problema, 
 
 	//end calling idai in order to get the max_bound to use
 	outfile<<"#!/bin/bash\n\n";
-	outfile<<"#PBS -N "<<GA_DEEP_NAME<<"\n\n#PBS -m a\n\n#PBS -M marvin.zarate@ufv.br\n\n#PBS -l walltime=00:30:00\n\n";
+	outfile<<"#PBS -N "<<GA_DEEP_NAME<<"\n\n";
+	//outfile<<"#PBS -m a\n\n";
+	//outfile<<"#PBS -M marvin.zarate@ufv.br\n\n";
+	outfile<<"#PBS -l walltime=00:30:00\n\n";
 
         outfile<<"source /usr/share/modules/init/bash\n\n";
         outfile<<"module load python\nmodule load mercurial\n\n";
@@ -112,8 +329,15 @@ void create_sh(string pasta, string dominio, string problema, int num_problema, 
 	outfile<<"RESULTS=/home/marvin/marvin/testss/"<<heuristic<<"/"<<PROB_PROBES<<"/"<<pasta.c_str()<<"/resultado"<<"\n\n";
 	//outfile<<"cd /home/marvin/fd\n\n";
 	outfile<<"cd ${DIR}\n\n";
-	outfile<<"python3 ${FD_ROOT}/src/translate/translate.py ${FD_ROOT}/benchmarks/"<<pasta.c_str()<<"/"<<dominio.c_str()<<" ${FD_ROOT}/benchmarks/"<<pasta.c_str()<<"/"<<problema.c_str()<<"\n\n";
-	outfile<<"${FD_ROOT}/src/preprocess/preprocess < output.sas"<<"\n\n";	
+
+
+	//outfile<<"python3 ${FD_ROOT}/src/translate/translate.py ${FD_ROOT}/benchmarks/"<<pasta.c_str()<<"/"<<dominio.c_str()<<" ${FD_ROOT}/benchmarks/"<<pasta.c_str()<<"/"<<problema.c_str()<<"\n\n";
+	//outfile<<"${FD_ROOT}/src/preprocess/preprocess < output.sas"<<"\n\n";	
+
+	string outputSA = translator(pasta.c_str(), problema.c_str());
+	cout<<"outputSA="<<outputSA<<"\n";
+	
+	/*
 			
 	outfile<<"${FD_ROOT}/src/search/downward-release  --global_probes "<<NUM_PROBES<<" --domain_name "<<pasta.c_str()<<" --domain_instance_pddl "<<dominio.c_str()<<"  --problem_name "<<problema.c_str()<<" --heuristic_name "<<heuristic<<" --search \"ss(min([lmcut(), ipdb(max_time=200), automate_GAs]))\" <  output > ${RESULTS}/"<<problema.c_str()<<"\n\n";
 	
@@ -122,7 +346,6 @@ void create_sh(string pasta, string dominio, string problema, int num_problema, 
         
 	outfile.close();
 
-	string date = currentDateTime();
 
 	string executeFile;
 	bool is_in_cluster = false;
@@ -139,7 +362,7 @@ void create_sh(string pasta, string dominio, string problema, int num_problema, 
         	system(allow.c_str());
         	executeFile = "timeout 1800 sh "+arquivo; //setting the limit time
         	system(executeFile.c_str());
-        }
+        }*/
 }
 
 void entrada_dados(string &pasta, string &problema, string &dominio, bool &dominio_unico, int &quantidade_problemas) {
@@ -192,9 +415,9 @@ void entrada_dados(string &pasta, string &problema, string &dominio, bool &domin
                 	}
 
 			ifstream file("h/ss/d/instance360_deep_probes.txt");
-			cout<<"heuristic = "<<heuristic<<"\n\n";
-			cout<<"quantidade_entrada_opt = "<<quantidade_entrada_opt<<"\n\n";
-			cout<<"total_heuristics = "<<total_heuristics<<"\n\n";
+			//cout<<"heuristic = "<<heuristic<<"\n\n";
+			//cout<<"quantidade_entrada_opt = "<<quantidade_entrada_opt<<"\n\n";
+			//cout<<"total_heuristics = "<<total_heuristics<<"\n\n";
 
 			string instances = "qstat | grep -c ";
 			instances += GA_DEEP_NAME;
@@ -206,7 +429,7 @@ void entrada_dados(string &pasta, string &problema, string &dominio, bool &domin
 				//cout<<"dominio = "<<dominio<<"\n";
 				int numero_problemas;
 				file>>quantidade_problemas;
-				cout<<"quantidade_problemas = "<<quantidade_problemas<<"\n";
+				//cout<<"quantidade_problemas = "<<quantidade_problemas<<"\n";
 		
 				if (dominio == "domain.pddl") {
 					dominio_unico = true;
